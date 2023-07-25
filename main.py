b47e38a5e91815f07219895ddd436ef5231eedc0
fanecovsf <gustavo_h07@hotmail.com>
2023-07-24 21:54:50 -0300

    🆕 Feature: Teste de migração para o SQLModel, mais sucinto e rápido.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_paginate import Pagination, get_page_args
-from models.models import db, Cliente, PlacasMinalbaMongo, PlacasMinalba
+#from models.models import db, Cliente, PlacasMinalbaMongo, PlacasMinalba
+from models.models2 import Cliente, PlacasMinalbaMongo, PlacasMinalba, engine_1, engine_2, engine_3, session_1, session_2, session_3
 from sqlalchemy import exc
 from sqlalchemy import event
 from threading import Thread
@@ -21,10 +22,8 @@ app.config['SQLALCHEMY_BINDS'] = {
 app.config['SQLALCHEMY_POOL_SIZE'] = 10
 app.config['SQLALCHEMY_POOL_TIMEOUT'] = 30
 
-db.init_app(app=app)
-
 with app.app_context():
-    @event.listens_for(db.engine, "engine_connect")
+    @event.listens_for(engine_1, "connect")
     def on_connect(dbapi_connection, connection_record):
         if isinstance(dbapi_connection, exc.DBAPIError):
             cursor = dbapi_connection.cursor()
@@ -43,11 +42,11 @@ def table():
     page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page', per_page='100')
 
     if search_term:
-        clientes = Cliente.query.filter(Cliente.nome.ilike(f'%{search_term}%')).offset(offset).limit(per_page).all()
-        total = Cliente.query.filter(Cliente.nome.ilike(f'%{search_term}%')).count()
+        clientes = session_1.query(Cliente).filter(Cliente.nome.ilike(f'%{search_term}%')).offset(offset).limit(per_page).all()
+        total = session_1.query(Cliente).filter(Cliente.nome.ilike(f'%{search_term}%')).count()
     else:
-        clientes = Cliente.query.offset(offset).limit(per_page).all()
-        total = Cliente.query.count()
+        clientes = session_1.query(Cliente).limit(per_page).offset(offset).all()
+        total = session_1.query(Cliente).count()
 
     pagination = Pagination(page=page, total=total, record_name='clientes', per_page=per_page, css_framework='bootstrap4')
 
@@ -56,11 +55,11 @@ def table():
 
 @app.route('/clientes/edit/<codigo>', methods=['GET', 'POST'])
 def edit(codigo):
-    cliente = Cliente.query.get(codigo)
+    cliente = session_1.query(Cliente).get(codigo)
     if request.method == 'POST':
         cliente.modelo_de_negocio = request.form['modelo']
         cliente.tipo_de_cliente = request.form['tipo_cliente']
-        db.session.commit()
+        session_1.commit()
         return redirect(url_for('table'))
     
     return render_template('edit.html', cliente=cliente)
@@ -72,11 +71,11 @@ def placas_minalba():
     page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page', per_page='100')
 
     if search_term:
-        placas = PlacasMinalba.query.filter(PlacasMinalba.placa.ilike(f'%{search_term}%')).offset(offset).limit(per_page).all()
-        total = PlacasMinalba.query.filter(PlacasMinalba.placa.ilike(f'%{search_term}%')).count()
+        placas = session_3.query(PlacasMinalba).filter(PlacasMinalba.placa.ilike(f'%{search_term}%')).offset(offset).limit(per_page).all()
+        total = session_3.query(PlacasMinalba).filter(PlacasMinalba.placa.ilike(f'%{search_term}%')).count()
     else:
-        placas = PlacasMinalba.query.offset(offset).limit(per_page).all()
-        total = PlacasMinalba.query.count()
+        placas = session_3.query(PlacasMinalba).offset(offset).limit(per_page).all()
+        total = session_3.query(PlacasMinalba).count()
 
     pagination = Pagination(page=page, total=total, record_name='placas', per_page=per_page, css_framework='bootstrap4')
 
@@ -85,10 +84,10 @@ def placas_minalba():
 
 @app.route('/placas-minalba/edit/<placa>', methods=['GET', 'POST'])
 def edit_placa(placa):
-    placa = PlacasMinalba.query.get(placa)
+    placa = session_3.query(PlacasMinalba).get(placa)
     if request.method == 'POST':
         placa.classificacao = request.form['classificacao']
-        db.session.commit()
+        session_3.commit()
         return redirect(url_for('placas_minalba'))
     
     return render_template('edit-placas-minalba.html', placa=placa)
@@ -98,15 +97,15 @@ def edit_placa(placa):
 def fill_placas_minalba():
     while True:
         time.sleep(7200)
-        placas_distintas = db.session.query(PlacasMinalbaMongo.idVeiculo).filter(PlacasMinalbaMongo.nomeEmbarcador != 'MINALBA' and PlacasMinalbaMongo.nomeEmbarcador != '').distinct().all()
+        placas_distintas = session_2.query(PlacasMinalbaMongo.idVeiculo).filter(PlacasMinalbaMongo.nomeEmbarcador != 'MINALBA' and PlacasMinalbaMongo.nomeEmbarcador != '').distinct().all()
 
         for placa in placas_distintas:
-            placa_existente = PlacasMinalba.query.filter_by(placa=placa[0]).first()
+            placa_existente = session_3.query(PlacasMinalba).filter_by(placa=placa[0]).first()
             if not placa_existente:
                 placa_nova = PlacasMinalba(placa=placa[0], classificacao=None)
-                db.session.add(placa_nova)
+                session_3.add(placa_nova)
 
-        db.session.commit()
+        session_3.commit()
 
 
 if __name__ == '__main__':
---
+++ models/models2.py
@@ -0,0 +1,43 @@
+from sqlmodel import Field, SQLModel, create_engine, Session
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+#Constantes
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
+DATABASE_URL1 = 'postgresql://postgres:pRxI65oIubsdTlf@4.228.57.67:5432/db_vibra'
+DATABASE_URL2 = 'postgresql://postgres:pRxI65oIubsdTlf@4.228.57.67:5432/db_minalba_mongodb'
+DATABASE_URL3 = 'postgresql://postgres:pRxI65oIubsdTlf@4.228.57.67:5432/db_minalba'
+
+engine_1 = create_engine(DATABASE_URL1)
+engine_2 = create_engine(DATABASE_URL2)
+engine_3 = create_engine(DATABASE_URL3)
+
+session_1 = Session(engine_1)
+session_2 = Session(engine_2)
+session_3 = Session(engine_3)
+
+class Cliente(SQLModel, table=True):
+    __tablename__ = 'tb_clientes'
+    __table_args__ = {'schema': 'sc_sap'}
+
+    codigo: str = Field(primary_key=True)
+    nome: str
+    modelo_de_negocio: str
+    tipo_cliente: str
+
+
+class PlacasMinalbaMongo(SQLModel, table=True):
+    __tablename__ = 'Viagens'
+    __table_args__ = {'schema': 'public'}
+
+    id: str = Field(primary_key=True)
+    idVeiculo: str
+    nomeEmbarcador: str
+
+
+class PlacasMinalba(SQLModel, table=True):
+    __tablename__ = 'tb_placas'
+    __table_args__ = {'schema': 'sc_placa'}
+
+    placa: str = Field(primary_key=True)
+    classificacao: str
+ 
