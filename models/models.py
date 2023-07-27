80dbc7c8ec3b1bc8a9c3bd2235654e9939054dd6
fanecovsf <gustavo_h07@hotmail.com>
2023-07-27 08:27:25 -0300

    🔁Refactor: Adicionado mais configurações para pools de conexão

--- models/models.py
+++ models/models.py
@@ -1,4 +1,9 @@
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy as _BaseSQLAlchemy
+
+class SQLAlchemy(_BaseSQLAlchemy):
+    def apply_pool_defaults(self, app, options):
+        super(SQLAlchemy, self).apply_pool_defaults(self, app, options)
+        options["pool_pre_ping"] = True
 
 db = SQLAlchemy()
 
