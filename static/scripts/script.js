ec2575e5e28c06067e83168bb5293063c82dd61f
nohranferreira <nohran1708@hotmail.com>
2023-08-07 17:14:49 -0300

    feat: adicionado dropdown para pesquisa de operação

---
+++ static/scripts/script.js
@@ -0,0 +1,8 @@
+const dropBusca = document.getElementById('drop')
+const tabela = document.getElementsByClassName('table-1')
+console.log(tabela)
+dropBusca.addEventListener('click', () => {
+    let valorClicado = dropBusca.value
+    
+    if (valorClicado === "Outbound") {}
+})
